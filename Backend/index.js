3c0090441310c9a01dba36f4008fd7c33cf6ce70
mkzmohanad <mkzmohanad@gmail.com>
2024-12-23 16:45:40 +0200

    test-2

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,3 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log('Connected to port successfully')
 })
-
-// username : mohanadshohdy633
-// password : B7TBIzaBMmKkzVPI
-// mongodb+srv://mohanadshohdy633:<db_password>@cluster0.rmc2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0

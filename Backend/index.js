1afeaeb0b82cf2354e771d4c80a00d748487a69c
mkzmohanad <mkzmohanad@gmail.com>
2024-12-25 16:09:46 +0200

    test-backend-5

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,27 @@ const dotenv = require('dotenv');
 dotenv.config({path : "./config.env"})
 const app = require('./app');
 
-const DB = process.env.DATABASE_STRING.replace("<db_password>" , `${process.env.DATABASE_password}`)
-
-mongoose.connect(DB).then(() => console.log('Connected to database...'))
+const connectDB = async () => {
+    try {
+      const DB = process.env.DATABASE_STRING.replace(
+        "<db_password>",
+        `${process.env.DATABASE_password}`
+      );
+  
+      await mongoose.connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      console.log('Connected to database...');
+    } catch (error) {
+      console.error('Database connection failed:', error.message);
+      process.exit(1);  // Exit the process with failure
+    }
+  };
+  
+  connectDB();
+  
 
 const port = process.env.PORT || 8000
 
